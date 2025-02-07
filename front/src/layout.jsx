b81b83c7c47bba21c14fa19a38832c43768735e7
RamyRxr <reyr06115@gmail.com>
2025-02-07 16:23:01 +0100

    Update the Settings and Interships

--- front/src/layout.jsx
+++ front/src/layout.jsx
@@ -12,7 +12,7 @@ import Events from "./pages/events/Events.jsx";
 import AddEvent from "./pages/addEvent/AddEvent.jsx";
 import Setting from "./pages/Settings/Settings.jsx";
 import Profil from "./pages/profil/Profil.jsx";
-import RequireAuth from "./components/shared/required.jsx";
+import RequireAuth from "./components/shared/RequireAuth.jsx";
 import { useState, useEffect } from "react";
 
 const Layout = () => {
